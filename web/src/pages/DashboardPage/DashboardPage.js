4032e29476d495a81f03e0268794b2780267fa20
Lue <escasinasluezyldominic@gmail.com>
2026-02-19 14:42:45 +0800

    feat(fe): - um ignore this

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../AuthManagement/AuthContext";
+
 export default function DashboardPage() {
+
+    const { userIsAuthenticated } = useContext(AuthContext);
+
     return (
         <div>
             <h1>Dashboard</h1>
